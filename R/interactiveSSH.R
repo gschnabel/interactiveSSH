7e0053ec0faa318a4fddd9d5b89110e70b941fe0
gschnabel <40870991+gschnabel@users.noreply.github.com>
2019-03-13 15:13:10 +0100

    added license preamble

--- R/interactiveSSH.R
+++ R/interactiveSSH.R
@@ -1,3 +1,18 @@
+#  interactiveSSH - sending shell commands over ssh
+#  Copyright (C) 2019  Georg Schnabel
+#  
+#  interactiveSSH is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  
+#  interactiveSSH is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>
 
 #' Initiate interactive SSH session
 #'
